324529e44910e451cdab69a9a57886e12cc58820
Justin Caringal <jacfriday10th@gmail.com>
2024-11-27 13:46:44 -0600

    Added multiprocessing Flask app execution to test

--- test_call.py
+++ test_call.py
@@ -1,6 +1,8 @@
 # Justin Caringal
 # A small test file to test the import of a Flask app into another file
 import webbrowser as wb
+from multiprocessing import Process
 from access_spotify import app
+server = Process(target=app.run)
+server.start()
 wb.open('http://127.0.0.1:5000/')
-app.run()
